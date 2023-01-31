bc7b3d52691990db2f3c40b22073cb406b50825d
John Davis <john.davis@airtime.com>
2023-01-31 15:52:14 -0800

    index.ts, place holder

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import {AirCoreCoordinate} from "../proto/generated/devinternal_pb";
+
 console.log("running")
+
+const coord = new AirCoreCoordinate({
+    kafkaTopic: "some.topic"
+});
+
+const bytes = coord.toBinary();
+const coord_2 = AirCoreCoordinate.fromBinary(bytes);
+
+console.log(`out: ${coord_2.kafkaTopic}`);
