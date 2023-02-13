f160369dbe90c44e29de6b9dc4e371d65394f5e3
John Davis <john.davis@airtime.com>
2023-02-13 09:49:34 -0800

    renames

--- src/examples/serde_test.ts
+++ src/examples/serde_test.ts
@@ -3,11 +3,11 @@ import {AirCoreFrame, Coordinates} from "../../proto/generated/devinternal_pb";
 console.log("running")
 
 const coord = new AirCoreFrame({
-    sendTo: new Coordinates({dbKey: {kafkaTopic: "topic"}})
+    sendTo: new Coordinates({kafkaKey: {kafkaTopic: "topic"}})
 });
 
 const bytes = coord.toBinary();
 const coord_2 = AirCoreFrame.fromBinary(bytes);
 
-const kafkaTopic = coord_2.sendTo?.dbKey?.kafkaTopic;
+const kafkaTopic = coord_2.sendTo?.kafkaKey?.kafkaTopic;
 if(kafkaTopic) console.log(`out: ${kafkaTopic}`);
