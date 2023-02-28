afdc0e8b2e3c36a1d4a0a7edb18d90e78cf6dd1a
John Davis <john.davis@airtime.com>
2023-02-28 09:04:09 -0800

    wip connect-grpc test

---
+++ src/connect/omni.impl.ts
@@ -0,0 +1,24 @@
+import {ConnectRouter, ServiceImpl} from "@bufbuild/connect";
+import {Omni} from "../../proto/gen/devinternal_connect";
+import {
+    GetDeltasRequest,
+    GetDeltasResponse,
+    GetSnapshotResponse,
+    Path,
+    UpsertRequest
+} from "../../proto/gen/devinternal_pb";
+import {Empty} from "@bufbuild/protobuf";
+
+export class OmniImpl implements ServiceImpl<typeof Omni> {
+    async upsert(request: UpsertRequest) {
+        return new Empty();
+    }
+
+    async getSnapshot(path: Path) {
+        return new GetSnapshotResponse();
+    }
+
+    async getDeltas(request: GetDeltasRequest) {
+        return new GetDeltasResponse();
+    }
+}
--- src/connect/server.test.ts
+++ src/connect/server.test.ts
@@ -1,7 +1,7 @@
 // server
 import { fastify } from "fastify";
 import { fastifyConnectPlugin } from "@bufbuild/connect-fastify";
-import routes from "./connect";
+import {OmniImpl} from "./omni.impl";
 
 // client
 import { createPromiseClient } from "@bufbuild/connect";
@@ -9,36 +9,31 @@ import { Omni } from "../../proto/gen/devinternal_connect";
 import { createConnectTransport } from "@bufbuild/connect-node";
 import {UpsertRequest} from "../../proto/gen/devinternal_pb";
 
-async function server() {
-    const server = fastify();
-
-    await server.register(fastifyConnectPlugin, {
-        routes,
-    });
-
-    server.get("/", (_, reply) => {
-        reply.type("text/plain");
-        reply.send("Hello World!");
-    });
-
-    await server.listen({ host: "localhost", port: 8080 });
-    console.log("server is listening at", server.addresses());
-}
-
-async function client() {
-    const transport = createConnectTransport({
-        baseUrl: "http://localhost:8080",
-        httpVersion: "1.1"
-    });
-
-    const client = await createPromiseClient(Omni, transport);
-    //const res = await client.say({ sentence: "I feel happy." });
-    const res = await client.upsert(new UpsertRequest());
-    console.log(res);
-}
-
 describe(`connect server`, () => {
     test(`simple test`, async () => {
+        const server = fastify();
+        try {
+            await server.register(fastifyConnectPlugin, {
+                routes(router) {
+                    router.service(Omni, new OmniImpl());
+                },
+            });
 
+            await server.listen({host: "localhost", port: 0});
+            console.log("server is listening at", server.addresses());
+
+            // client
+            const transport = createConnectTransport({
+                baseUrl: `http://localhost:${server.addresses()[0].port}`,
+                httpVersion: "1.1"
+            });
+
+            const client = await createPromiseClient(Omni, transport);
+            const res = await client.upsert(new UpsertRequest());
+
+            console.log(`upsert: `, res);
+        } finally {
+            await server.close();
+        }
     })
 })
