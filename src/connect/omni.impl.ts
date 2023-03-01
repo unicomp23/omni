eb1c2605e9ac057d3fce06221feb9c1916a52e8e
John Davis <john.davis@airtime.com>
2023-03-01 15:03:45 -0800

    wip grpc test

--- src/connect/omni.impl.ts
+++ src/connect/omni.impl.ts
@@ -8,14 +8,36 @@ import {
     UpsertRequest
 } from "../../proto/gen/devinternal_pb";
 import {Empty} from "@bufbuild/protobuf";
+import {config} from "../config";
+import {AsyncDisposableStack} from "@esfx/disposable";
+import {Deferred} from "@esfx/async";
+import {anydb} from "../common/redis/anydb";
+import {pubsub} from "../easy/pubsub";
+import {createClient} from "redis";
 
 export class OmniImpl implements ServiceImpl<typeof Omni> {
+    private constructor(
+        private readonly config_: config,
+        private readonly disposable_stack: AsyncDisposableStack,
+        private readonly shutdown: Deferred<boolean>,
+        private readonly anydb_: anydb,
+        private readonly pubsub_: pubsub,
+    ) {
+    }
+
+    public static async create(config_: config, disposable_stack: AsyncDisposableStack, shutdown: Deferred<boolean>) {
+        const anydb_ = await anydb.create(createClient({url: config_.get_redis_uri()}));
+        const pubsub_ = await pubsub.create(config_);
+        const omni = new OmniImpl(config_, disposable_stack, shutdown, anydb_, pubsub_);
+        return omni;
+    }
+
     async upsert(request: UpsertRequest) {
         console.log(`got upsert request`);
         return new Empty();
     }
 
-    async getSnapshot(path: Path) {
+    async getSnapshot(sequence_number_path: Path) {
         return new GetSnapshotResponse();
     }
 
--- src/connect/omni.test.ts
+++ src/connect/omni.test.ts
@@ -8,21 +8,28 @@ import {createPromiseClient} from "@bufbuild/connect";
 import {Omni} from "../../proto/gen/devinternal_connect";
 import {createConnectTransport} from "@bufbuild/connect-node";
 import {UpsertRequest} from "../../proto/gen/devinternal_pb";
+import {config} from "../config";
+import {AsyncDisposableStack} from "@esfx/disposable";
+import {Deferred} from "@esfx/async";
 
 describe(`connect server`, () => {
     test(`simple test`, async () => {
         const server = fastify();
+        const config_ = config.create();
+        const disposable_stack = new AsyncDisposableStack();
+        const shutdown = new Deferred<boolean>();
         try {
+            const omni_ = await OmniImpl.create(config_, disposable_stack, shutdown);
             await server.register(fastifyConnectPlugin, {
                 routes(router) {
-                    router.service(Omni, new OmniImpl());
+                    router.service(Omni, omni_);
                 },
             });
 
             await server.listen({host: "localhost", port: 0});
             console.log("server is listening at", server.addresses());
 
-            // client
+            // client side testing
             const transport = createConnectTransport({
                 baseUrl: `http://localhost:${server.addresses()[0].port}`,
                 httpVersion: "1.1"
@@ -32,8 +39,10 @@ describe(`connect server`, () => {
             const res = await client.upsert(new UpsertRequest());
 
             console.log(`upsert: `, res);
+            expect(await shutdown.promise).toBe(true);
         } finally {
             await server.close();
+            await disposable_stack.disposeAsync();
         }
     })
 })
