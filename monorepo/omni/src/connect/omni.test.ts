f0284d564214fbfde0cdc8dbc3d439a6fda05608
John Davis <john.davis@airtime.com>
2023-03-02 15:35:13 -0800

    sequencing checks

--- monorepo/omni/src/connect/omni.test.ts
+++ monorepo/omni/src/connect/omni.test.ts
@@ -63,7 +63,7 @@ describe(`connect server`, () => {
             // get snap
             let keep_running = true;
             let deltasStartSequenceNumber = BigInt(0);
-            while(keep_running) {
+            while (keep_running) {
                 await delay(100);
                 const snapshot = await client.getSnapshot(paths.sequence_number_path);
                 for (const payload of snapshot.payloads) {
@@ -82,7 +82,7 @@ describe(`connect server`, () => {
             //console.log(`omni.test.snapshot: `, snapshot.toJsonString({prettySpaces: 2}));
 
             // add more deltas
-            for(let i = 0; i < delta_count; i++) {
+            for (let i = 0; i < delta_count; i++) {
                 const empty_2 = await client.upsert(new UpsertRequest({
                     payload: {
                         x: {
@@ -98,31 +98,33 @@ describe(`connect server`, () => {
             // fetch deltas, relative to snap
             keep_running = true;
             let delta_index = 0;
-            while(keep_running) {
+            const endSequenceNumber = deltasStartSequenceNumber + BigInt(delta_count);
+            let currentSequenceNumber = deltasStartSequenceNumber;
+            while (keep_running) {
                 await delay(100);
                 const delta = await client.getDeltas(new GetDeltasRequest({
                     sequenceNumberPath: paths.sequence_number_path,
                     sequenceNumber: deltasStartSequenceNumber,
                 }));
-                if(delta) {
-                    if(delta.payloads) {
-                        for(const payload of delta.payloads) {
-                            if(payload) {
-                                expect(payload.x.case).toBe(`text`);
-                                expect(payload.x.value).toBe(delta_index.toString());
-                                delta_index++;
-                                if(delta_index == delta_count) {
-                                    keep_running = false;
-                                    accumulated_checks_count++;
-                                    break;
-                                }
+                if (delta.payloads) {
+                    for (const payload of delta.payloads) {
+                        if (payload && payload?.sequencing?.sequenceNumber) {
+                            expect(payload.sequencing.sequenceNumber === currentSequenceNumber)
+                            currentSequenceNumber++;
+                            expect(payload.x.case).toBe(`text`);
+                            expect(payload.x.value).toBe(delta_index.toString());
+                            delta_index++;
+                            if (delta_index == delta_count) {
+                                keep_running = false;
+                                accumulated_checks_count++;
+                                shutdown_server.resolve(true);
+                                break;
                             }
                         }
                     }
                 }
             }
 
-            shutdown_server.resolve(true); // todo
             expect(await shutdown_server.promise).toBe(true);
         } finally {
             await server.close();
