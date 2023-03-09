97af72af56b25062d185e74e5976333d094bbc86
John Davis <john.davis@airtime.com>
2023-03-08 16:52:23 -0800

    omni server endpoint

---
+++ monorepo/omni/src/server/main.ts
@@ -0,0 +1,39 @@
+import {fastify} from "fastify";
+import {config} from "../config";
+import {AsyncDisposableStack} from "@esfx/disposable";
+import {Deferred} from "@esfx/async";
+import {spawn_server} from "../easy/servers/late.join.server";
+import {OmniImpl} from "../connect/omni.impl";
+import {fastifyConnectPlugin} from "@bufbuild/connect-fastify";
+import {Omni} from "../../proto/gen/devinternal_connect";
+
+async function main() {
+    console.log(`starting ...`);
+    const server = fastify();
+    const config_ = config.create();
+    const disposable_stack = new AsyncDisposableStack();
+    const shutdown_server = new Deferred<boolean>();
+    const worker_ = spawn_server(config_, disposable_stack, shutdown_server);
+    disposable_stack.use(worker_);
+    try {
+        const omni_ = await OmniImpl.create(config_, disposable_stack, shutdown_server);
+        await server.register(fastifyConnectPlugin, {
+            routes(router) {
+                router.service(Omni, omni_);
+            },
+        });
+
+        await server.listen({host: "localhost", port: 80});
+        console.log("server is listening at", server.addresses());
+
+        console.log(`waiting on shutdown signal`);
+        await shutdown_server.promise; // todo signals
+        console.log(`shutdown signal received`);
+    } finally {
+        await disposable_stack.disposeAsync();
+    }
+}
+
+main().then(() => {
+    console.log(`main.exit`);
+})
